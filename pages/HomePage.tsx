b294601149e334cdd1e3544b2f4353a950ca410a
Shaun Mendes <shaunkmmendes@gmail.com>
2023-11-02 21:02:27 +0530

    change

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -24,7 +24,7 @@ const HomePage: React.FC = () => (
     <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
     <div className="lg:w-[570px]">
     <h1 className="text-[36px] leading-[46px] text*-headingColor font-[800] md:text-[60px] md:leading-[70px]">
-    We help patients live a healthy, longer life.
+    We help patients live a healthy and longer life.
     </h1>
     <div className="pt-[60px] 2xl:h-[800px]">
     <p className="text__para">
